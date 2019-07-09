19ea3cd8d506bc92704fc19bc42dd5b1a3b548df
Danielle Subang <daniellesubang@gmail.com>
2019-07-09 01:33:12 -0700

    Extracted wrapper function- react hooks set state

--- src/Components/ChatsWrapper.jsx
+++ src/Components/ChatsWrapper.jsx
@@ -80,7 +80,7 @@ export default function ChatsWrapper(props) {
         className="col"
         style={{
           overflow: "auto",
-          maxHeight: "500px"
+          maxHeight: "560px"
         }}
       >
         <div className={clsx(!open && classes.hide)}>
